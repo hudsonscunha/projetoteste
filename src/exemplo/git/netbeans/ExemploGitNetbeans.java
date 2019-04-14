8a04703f7ce6168b4932daa62e0fc5c5db6acdb7
hudson <hudson.contato@hotmail.com>
2019-04-14 17:49:04 -0300

    Outro commit

--- src/exemplo/git/netbeans/ExemploGitNetbeans.java
+++ src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -18,6 +18,7 @@ public class ExemploGitNetbeans {
     public static void main(String[] args) {
         System.out.println("Exemplo de um projeto com Git e NetBeans");
         //Alterado da web
+        //Alterado do netbeans
     }
 
 }
